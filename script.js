15f2c08d0c08654614a9f4ea553fc6c47c0c3ae2
Robelek <92119683+Robelek@users.noreply.github.com>
2021-10-07 21:10:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let numer1= "";
+let numer2= "";
+let wynik ="";
+let wybieranumer2 = false;
+let dzialanie = "";
+function wybierz(a)
+{
+	if(!wybieranumer2)
+	{
+		numer1+=a;
+		document.getElementById("wynik").innerHTML = numer1;
+	}
+	else
+	{
+		numer2+=a;
+		document.getElementById("wynik").innerHTML = numer2;
+	}
+}
+function dzialanko(a)
+{
+	wybieranumer2 = true;
+	dzialanie=a;
+}
+
+
+function wyswietl()
+{
+	numer1= +numer1;
+	numer2= +numer2;
+	if(dzialanie=="-")
+	{
+		wynik = numer1-numer2;
+		
+	}
+	else if(dzialanie =="+")
+	{
+		wynik =numer1+numer2;
+	}
+	else if(dzialanie =="*")
+	{
+		wynik =numer1*numer2;
+	}
+	else if(dzialanie =="/")
+	{
+		wynik =numer1/numer2;
+	}
+	else if (dzialanie =="")
+	{
+		wynik = "BLAD";
+	}
+	document.getElementById("wynik").innerHTML = wynik;
+	//alert(numer1);
+	numer1 = "";
+	numer2= "";
+	dzialanie="";
+	wybieranumer2 = false;
+}
+	
